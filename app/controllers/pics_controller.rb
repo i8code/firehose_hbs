205a10c70048b8ac89a1b4e2a4eac9c757d86de6
i8code <rsteinbergis@gmail.com>
2013-11-16 17:23:46 -0500

    Added new page; create pics controll; added form;

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -6,4 +6,30 @@ class PicsController < ApplicationController
 		@pics = Pic.all
 
 	end 
+
+	def new
+
+		#building a blank thng, filling it out on the form
+		@pic = Pic.new
+
+
+	end
+
+	#do...end, def...end
+	def create 
+			@pic = Pic.create( pic_params )
+			redirect_to pics_path
+
+	end
+
+#function 
+private
+#slurp up these paramaters
+def pic_params
+	# permit the user to upload these things into your site 
+	params.require(:pic).permit(:emotion, :description, :location)
+
+	end
+
+
 end
