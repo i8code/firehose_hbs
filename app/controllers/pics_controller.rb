ef99f4417777677bb3470ababad6d69d501ba9ec
i8code <rsteinbergis@gmail.com>
2013-11-17 14:07:57 -0500

    Added filter; added verification

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -3,29 +3,39 @@ class PicsController < ApplicationController
 	def index 
 
 		#get the value and store it into the database
-		@pics = Pic.all
+		
+		#addes filter
+		if params[:place].present?
+			@pics = Pic.where(:place => params[:place])
+		else
+			@pics = Pic.all
+		end
 
 	end 
 
 	def new
 
 		#building a blank thng, filling it out on the form
+		
 		@pic = Pic.new
-
-
 	end
 
 	#do...end, def...end, passes all parameters from the simple form into this array
 	def create 
-			@pic = Pic.create( pic_params )
+		@pic = Pic.create( pic_params )
+		#add validation 
+		if @pic.persisted?
 			redirect_to pics_path
 
+		else
+			render :new, :status => :unprocessable_entity
+		end
 	end
 
-#function 
-private
-#slurp up these paramaters
-def pic_params
+	#function 
+	private
+	#slurp up these paramaters
+	def pic_params
 	# permit the user to upload these things into your site 
 	params.require(:pic).permit(:place, :description, :location, :image)
 
--- app/models/pic.rb
+++ app/models/pic.rb
@@ -3,4 +3,10 @@ class Pic < ActiveRecord::Base
 	#ImageUploader from the uploaders file 
 	mount_uploader :image, ImageUploader
 
+	# validate that the user added text
+	validates :image, :presence => true
+	validates :place, :presence => true
+	validates :description, :presence => true
+	validates :location, :presence => true
+
 end
