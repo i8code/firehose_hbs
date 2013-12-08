6c1b861eae9f2d1d5d95989a70176df4b7b37b6e
i8code <rsteinbergis@gmail.com>
2013-12-08 11:18:54 -0500

    cleaned up code

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,37 +1,26 @@
 class PicsController < ApplicationController
-
 	def index 
-
 		#get the value and store it into the database
-		
 		#addes filter
 		if params[:place].present?
 			@pics = Pic.where(:place => params[:place])
 		else
 			@pics = Pic.all
 		end
-
 	end 
 
 	def show
-#get the value and store it into the datab
+	#get the value and store it into the datab
 		@picsAll = Pic.all
-
 		if params[:id].present?
 			@pic = Pic.find_by_id(params[:id])
-		
 			# show all places nearby 
 			@picsNearby = Pic.within(1, origin: @pic)
 		end
-			
-			
-
 	end
 
 	def new
-
-		#building a blank thng, filling it out on the form
-		
+		#building a blank item, filling it out on the form
 		@pic = Pic.new
 	end
 
@@ -41,7 +30,6 @@ class PicsController < ApplicationController
 		#add validation 
 		if @pic.persisted?
 			redirect_to pics_path
-
 		else
 			render :new, :status => :unprocessable_entity
 		end
@@ -51,16 +39,13 @@ class PicsController < ApplicationController
 	private
 	#slurp up these paramaters
 	def pic_params
-	# permit the user to upload these things into your site 
-	params.require(:pic).permit(:place, :description, :location, :image)
+		# permit the user to upload these things into your site 
+		params.require(:pic).permit(:place, :description, :location, :image)
+	end
 
-end
-
-	#added
+	#added for maps
 	def map
 		#loads most recent place, always pull the last row in the table
 		@location = Location.last
 	end
-
-
 end
